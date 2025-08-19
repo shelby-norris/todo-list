ebe36fb3a586768783a5edf8c17b3025af5b4f18
Shelby Norris <shelbs9109@gmail.com>
2025-08-18 20:07:12 -0500

    installed vite tailwind for styling, edited code in App.jsx tto include code template from assignment, connected dynamodb npm, ensured app was connected to dynamo db app successfully and will add to do items and update in the database

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,92 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+DynamoDBDocumentClient,
+ScanCommand,
+PutCommand
+} from '@aws-sdk/lib-dynamodb';
+
+const client = new DynamoDBClient({
+region: import.meta.env.VITE_AWS_REGION,
+credentials: {
+accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
+secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+}
+});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+const TABLE_NAME = "Todo"
+
 
 function App() {
 
-  return (
-    <>
+const [todos, setTodos] = useState([]); // the array where scanCommand will save the information
+const [text, setText] = useState(''); // string that is representing the text that you want to save in the table
 
-    </>
-  )
+const scanTodos = async () => {
+
+try {
+const command = new ScanCommand({ TableName: TABLE_NAME })
+const response = await docClient.send(command);
+console.log(response)
+setTodos(response.Items)
+} catch (err) {
+console.log(err.message)
+}
+
+
+}
+
+const createTodo = async () => {
+
+
+const item = {
+id: Date.now().toString(),
+Text : text,
+isComplete : false
+}
+
+const command = new PutCommand({ TableName: 'Todo', Item: item })
+const response = await docClient.send(command);
+console.log(response)
+
+}
+
+// The useEffect hook is called every time the component is showed to the user
+// onload event on html
+useEffect(() => {
+scanTodos()
+}, []);
+
+const changeHandlerText = (event)=>{
+
+const data =event.target.value
+setText(data)
+
+}
+
+return (
+<>
+<div style={{ padding: 20 }}>
+<h1 className='text-3xl font-bold text-white'>Todo App</h1>
+<input
+value={text}
+onChange={changeHandlerText }
+style={{ marginRight: 8 }}
+className='variant-outlined bg-gray-600 rounded-lg  text-white'
+/>
+ 
+<button onClick={createTodo} className='text-white' >Send Data</button>
+
+<ul className="text-white" style={{ marginTop: 16 }}>
+{todos.map(t => (
+<li key={t.id}>{t.Text}</li>
+))}
+</ul>
+</div>
+</>
+)
 }
 
 export default App
